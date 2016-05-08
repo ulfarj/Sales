1b5b158106edb5ec0a1b4e2e6e3ecf706b69c69c
ulfarj <ulfarj@gmail.com>
2016-05-08 15:06:26 +0000

    ss

--- src/components/cells/StatusCell.js
+++ src/components/cells/StatusCell.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Input } from 'react-bootstrap';
 import {Table, Column, Cell} from 'fixed-data-table';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 class StatusCell extends Component {
 
@@ -11,28 +12,40 @@ class StatusCell extends Component {
 
     if(!loaded) {return(<Cell></Cell>);}
 
-    let x = 0;
-    let cx = 0;
-    let statusIcons = companies[rowIndex].sales.map(sale => {
-
+    let sales = companies[rowIndex].sales.map(sale => {
       let selected = _.indexOf(filter.categories, sale.categoryId) > -1;
       let category = _.find(this.props.categories, ['_id', sale.categoryId]);
       let status = _.find(this.props.statuses, ['_id', sale.statusId]);
       let salesman = _.find(this.props.salesmen, ['_id', sale.salesmanId]);
-      
+
+      return {
+        'selected': selected,
+        'category': category.name,
+        'status': status.name,
+        'salesman': salesman.name,
+        'color': status.color
+      }
+    });
+
+    sales = _.sortBy(sales, function(o) { return o.category; });
+
+    let x = 0;
+    let cx = 0;
+    let statusIcons = sales.map(sale => {
+
       x = x + 15;
-      if(selected)
+      if(sale.selected)
       {
         return (
-          <rect width="12" height="12" x={x} y="3" stroke="black" strokeWidth="1" fill={status.color}>
-            <title>{category.name} - {salesman.name} - {status.name}</title>
+          <rect width="12" height="12" x={x} y="3" stroke="black" strokeWidth="1" fill={sale.color}>
+            <title>{sale.category} - {sale.salesman} - {sale.status}</title>
           </rect>
           );
       }
 
       return (
-        <rect width="12" height="12" x={x} y="3" rx={8} ry={8} stroke="black" strokeWidth="1" fill={status.color}>
-          <title>{category.name} - {salesman.name} - {status.name}</title>
+        <rect width="12" height="12" x={x} y="3" rx={8} ry={8} stroke="black" strokeWidth="1" fill={sale.color}>
+          <title>{sale.category} - {sale.salesman} - {sale.status}</title>
         </rect>
         );
     });
