9f97a50d447db8dda9b5fba4a14a0755d221af8a
ulfarj <ulfarj@gmail.com>
2019-06-19 18:52:39 +0000

    t

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import Main from './components/main';
 import configureStore from './store/configureStore';
 import Routes from './routes';
 
 const store = configureStore();
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Routes store={store} />
+//   </Provider>,
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes store={store} />
-  </Provider>,
+  <div>Hello</div>,
   document.getElementById('app')
 );
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import moment from 'moment';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import App from './containers/app';
-import Main from './components/main';
-import Login from './components/login';
-import Reports from './containers/Reports';
-import Admin from './containers/Admin';
-import { authenticated, notAuthenticated } from './actions/account';
-import { updateSize } from './actions/common';
+// import App from './containers/app';
+// import Main from './components/main';
+// import Login from './components/login';
+// import Reports from './containers/Reports';
+// import Admin from './containers/Admin';
+// import { authenticated, notAuthenticated } from './actions/account';
+// import { updateSize } from './actions/common';
 
 class Routes extends React.Component {
 
